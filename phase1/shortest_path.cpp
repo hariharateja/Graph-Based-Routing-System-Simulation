c068e2038ca29a7fa20ca04d228f4b61972ebbca
hariharateja <hariharateja1977@gmail.com>
2025-11-01 17:14:52 +0530

    defined struct for shortest path

---
+++ phase1/shortest_path.cpp
@@ -0,0 +1,7 @@
+#include "shortest_path.hpp"
+#include <queue>   
+#include <map>     
+
+ShortestPathResult findShortestPath(const Graph& graph, const json& query){
+    
+}
---
+++ phase1/shortest_path.hpp
@@ -0,0 +1,32 @@
+#include<nlohmann/json.hpp>
+
+#include"graph.hpp"
+#include <string>
+#include <vector>
+
+using json = nlohmann::json;
+
+struct ShortestPathResult {
+    int id;
+    bool possible;
+    double minimum_distance;
+    double minimum_time;
+    
+    std::vector<int> path;
+    json toJson(const std::string& mode) const {
+        json result;
+        result["id"] = id;
+        result["possible"] = possible;
+        if (possible) {
+            if (mode == "distance") {
+                result["minimum_distance"] = minimum_distance;
+            } else {
+                result["minimum_time"] = minimum_time;
+            }
+            result["path"] = path;
+        }
+        return result;
+    }
+};
+
+ShortestPathResult findShortestPath(const Graph& graph, const json& query);
