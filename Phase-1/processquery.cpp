3a942927643c4d3b9579eeb4fc52983acc2544fc
bala <mbalaaditya@gmail.com>
2025-11-05 17:48:50 +0000

    Remove unnecessary stf

--- Phase-1/processquery.cpp
+++ Phase-1/processquery.cpp
@@ -22,34 +22,3 @@ json process_query(const json& event){
     // Unknown event type: return an error JSON
     return json{{"error", "unknown_event_type"}};
 }
-// static std::string formatJson(const json& j, int indent = 0, const std::string& key = "") {
-//     std::string ind(indent, ' ');
-//     if (j.is_object()) {
-//         std::string out = "{\n";
-//         bool first = true;
-//         for (auto it = j.begin(); it != j.end(); ++it) {
-//             if (!first) out += ",\n";
-//             first = false;
-//             out += std::string(indent + 4, ' ') + "\"" + it.key() + "\": ";
-//             out += formatJson(it.value(), indent + 4, it.key());
-//         }
-//         out += "\n" + ind + "}";
-//         return out;
-//     }
-//     else if (j.is_array()) {
-        
-//         if (key == "path" || key == "nodes") {
-//         return j.dump();
-//         }
-//         if (j.empty()) return "[]";
-//         std::string out = "[\n";
-//         for (size_t i = 0; i < j.size(); ++i) {
-//             if (i) out += ",\n";
-//             out += std::string(indent + 4, ' ') + formatJson(j[i], indent + 4, "");
-//         }
-//         out += "\n" + ind + "]";
-//         return out;
-//     }
-//     // primitives and strings: use dump() to get correct quoting
-//     return j.dump();
-// }
