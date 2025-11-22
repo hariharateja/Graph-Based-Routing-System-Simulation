2bb1961e3b48e1127a6fc72862819463e3860975
sandy <brssandesh@gmail.com>
2025-11-22 12:28:36 +0530

    and this continues...

--- Phase-2/utility.cpp
+++ Phase-2/utility.cpp
@@ -1,5 +1,7 @@
  #include "utility.hpp"
 
+static const double R = 6371000.0;
+
 //to identify unique paths [N:1,4,7|E:10,12]
 std::string makePathSignature(const Path& p) {
     std::ostringstream oss;
@@ -17,12 +19,27 @@ std::string makePathSignature(const Path& p) {
     return oss.str();
 }
 
+double toRad(double deg) {
+    return deg * M_PI / 180.0;
+}
+
 double heuristic(const Graph& graph, int node, int target){
     const Node &n1 = graph.getNode(node);
     const Node &n2 = graph.getNode(target);
     
-    double dx = n1.lat - n2.lat;
-    double dy = n1.lon - n2.lon;
-    return std::sqrt(dx * dx + dy * dy);
+    double lat1 = toRad(n1.lat);
+    double lon1 = toRad(n1.lon);
+    double lat2 = toRad(n2.lat);
+    double lon2 = toRad(n2.lon);
+
+    double dlat = lat2 - lat1;
+    double dlon = lon2 - lon1;
+    
+    double a = std::sin(dlat / 2) * std::sin(dlat / 2) + std::cos(lat1) * std::cos(lat2) * std::sin(dlon / 2) * std::sin(dlon / 2);
+
+    double c = 2 * std::atan2(std::sqrt(a), std::sqrt(1 - a));
+
+    double d = R * c;
+    return d;
 }
 
